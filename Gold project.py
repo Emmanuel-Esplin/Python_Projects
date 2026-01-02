29689c260ea526762596c62b2c818375455b93b5
Emmanuel <58236392@mylife.unisa.ac.za>
2026-01-02 20:50:39 +0200

    Add gold project

---
+++ Gold project.py
@@ -0,0 +1,36 @@
+# Import all packages
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import xgboost as xgb
+
+import warnings
+warnings.filterwarnings("ignore")
+sns.set_style("darkgrid", {"grid.color": ".6",
+                            "grid.linestyle": ":"})
+
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.pipeline import make_pipeline
+from sklearn.linear_model import Lasso
+
+from sklearn.ensemble import RandomForestRegressor
+from xgboost import XGBRegressor
+from sklearn.metrics import r2_score
+from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import GridSearchCV
+
+# Load dataset
+dataset = pd.read_csv("gold_price_data.csv", parse_dates=["Date"])
+
+# Info about the dataset
+dataset.info()
+
+# Null values in the dataset
+dataset.isna().sum().sort_values(ascending=False) 
+
+
+
+
